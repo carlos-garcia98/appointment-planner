b7bebd56a1833fe35df74726473f9e8047e1bdee
carlos-garcia98 <carlos0698@live.com>
2024-12-25 17:37:53 -0600

    Add ContactForm to be displayed as well as the contacts list

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -7,10 +7,32 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input value={name} onChange={(e) => setName(e.target.value)}></input>
+        </label>
+        <label>
+          Phone:
+          <input
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+          ></input>
+        </label>
+        <label>
+          Email:
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></input>
+        </label>
+        <button type="submit">Add Contact</button>
+      </form>
+    </>
   );
 };
-
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -3,11 +3,15 @@ import React, { useState, useEffect } from "react";
 import { ContactForm } from "../../components/contactForm/ContactForm";
 import { TileList } from "../../components/tileList/TileList";
 
-export const ContactsPage = () => {
+export const ContactsPage = ({ contacts, addContact }) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const [name, setName] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [duplicate, setDuplicate] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,21 +19,43 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if (!duplicate) {
+      addContact(name, phoneNumber, email);
+      setName("");
+      setPhoneNumber("");
+      setEmail("");
+    } else {
+      alert("Contact already exists!");
+    }
   };
 
   /*
   Using hooks, check for contact name in the 
   contacts array variable in props
   */
+  useEffect(() => {
+    const nameExists = contacts.some((contact) => contact.name === name);
+    setDuplicate(nameExists);
+  }, [name, contacts]);
 
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm
+          name={name}
+          setName={setName}
+          phone={phoneNumber}
+          setPhone={setPhoneNumber}
+          email={email}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+        />
       </section>
       <hr />
       <section>
         <h2>Contacts</h2>
+        <TileList contacts={contacts} />
       </section>
     </div>
   );
